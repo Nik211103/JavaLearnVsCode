25f9189fc1c60c94e9c3dff752de4164eaf3c20c
Nikita <mitichkin0303@mail.ru>
2023-11-29 21:28:32 +0200

    Аналогично

--- program.java
+++ program.java
@@ -1,6 +1,7 @@
 ﻿/**
  * program
  */
+import java.util.Scanner;
 public class program {
     
     // public static void main(String[] args) {
@@ -31,14 +32,32 @@ public class program {
     //     System.out.println(flag2);
     //     System.out.println(flag3); 
 
+    // public static void main(String[] args) {
+    //     var i = 123;
+    //     System.out.println(i);
+    //     int a = 123;
+    //     a++;
+    //     System.out.println(a);
+    //     System.out.println(++a);//можно так
     // }
-    public static void main(String[] args) {
-        var i = 123;
-        System.out.println(i);
-        int a = 123;
-        a++;
-        System.out.println(a);
-        System.out.println(++a);//можно так
-    }
+
+        // public static void main(String[] args) {
+            
+        //     int[] arr = new int[10];
+        //     arr[3] = 13;
+        //     arr = new int[] {1, 2, 3, 4, 5};
+        //     System.out.println(arr[3]);
+        //     System.out.println(arr.length);
+        //     //Есть понятия явное и неявное преобразование
+
+        // }
+        public static void main(String[] args) {
+            Scanner iScanner = new Scanner(System.in);
+            System.out.println("name: ");
+            String name = iScanner.nextLine();
+            System.out.printf("Привет, %s!", name);
+            iScanner.close();
+
+        }
 }
     
