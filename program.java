7fe17ca8635e6e0d89a58175784fc8a906c107e1
Nikita <mitichkin0303@mail.ru>
2023-11-29 10:50:32 +0200

    Учусь Java

---
+++ program.java
@@ -0,0 +1,40 @@
+﻿/**
+ * program
+ */
+public class program {
+    
+    // public static void main(String[] args) {
+    //     String s = "  2";
+    //     System.out.println(s);
+    // }
+
+    // public static void main(String[] args) {
+    //     int age = 8;
+    //     short salary = 12;
+
+    //     System.out.println(age);
+    //     System.out.println(salary);
+
+
+    //     float f = 123.45f;
+    //     double a = 123.45;
+
+    //     System.out.println(f);
+    //     System.out.println(a);
+
+    //     boolean flag1 = true && false;
+    //     boolean flag2 = true || false;
+
+    //     boolean flag3 = flag1 ^ flag2;
+
+    //     System.out.println(flag1);
+    //     System.out.println(flag2);
+    //     System.out.println(flag3); 
+
+    // }
+
+    public static void main(String[] args) {
+        var i = 123;
+        System.out.println(i);
+    }
+}
