5fa97e62d280bd2ec72336fb510c5fed76412655
Nikita <mitichkin0303@mail.ru>
2023-12-01 23:24:44 +0200

    Работаю с семинаром 1 по Java

---
+++ ПервыйСеминар/program.java
@@ -0,0 +1,180 @@
+﻿package ПервыйСеминар;
+/**
+ * program
+ */
+//import java.util.Scanner;
+//public class program {
+    // public static void main(String[] args) {
+    //     Scanner iScanner = new Scanner(System.in);
+    //     System.out.println("name: ");
+    //     String name = iScanner.nextLine();
+    //     System.out.printf("Привет, %s!", name);
+    //     iScanner.close();
+    
+    // }
+    
+    // public static void main(String[] args) {
+    //     String s = "  2";
+    //     System.out.println(s);
+    // }
+
+    // public static void main(String[] args) {
+    //     int age = 8;
+    //     short salary = 12;
+
+    //     System.out.println(age);
+    //     System.out.println(salary);
+
+
+    //     float f = 123.45f;
+    //     double a = 123.45;
+
+    //     System.out.println(f);
+    //     System.out.println(a);
+
+    //     boolean flag1 = true && false;
+    //     boolean flag2 = true || false;
+
+    //     boolean flag3 = flag1 ^ flag2;
+
+    //     System.out.println(flag1);
+    //     System.out.println(flag2);
+    //     System.out.println(flag3); 
+
+    // public static void main(String[] args) {
+    //     var i = 123;
+    //     System.out.println(i);
+    //     int a = 123;
+    //     a++;
+    //     System.out.println(a);
+    //     System.out.println(++a);//можно так
+    // }
+
+        // public static void main(String[] args) {
+            
+        //     int[] arr = new int[10];
+        //     arr[3] = 13;
+        //     arr = new int[] {1, 2, 3, 4, 5};
+        //     System.out.println(arr[3]);
+        //     System.out.println(arr.length);
+        //     //Есть понятия явное и неявное преобразование
+
+        // }
+
+        // static void sayHi() {
+        //     System.out.println("hi!");
+        // }
+        // static int sum(int a , int b) {
+        //     return a+b;
+        // }
+        // static double factor(int n){
+        // if(n==1) return 1;
+        // return n * factor(n-1);
+        // }
+        // public static void main(String[] args) {
+        //     sayHi();
+        //     System.out.println(sum(1, 3));
+        //     System.out.println((factor(5)));
+
+        //     int a = 1;
+        //     int b = 2;
+        //     int c;
+        //     if (a > b) {  //if (a > b ) c = a;
+        //         c = a;
+        //     } else{       //if (b > a) c = b;
+        //         c = b;
+        //     }
+        //     System.out.println(c);;
+        // }
+            // public static void main(String[] args) {
+            //     int a = 123;
+            //     a = a - 122;
+            //     switch (a) {
+            //         case 1: System.out.println(a);
+            //             break;
+            //         case 10: System.out.println('b');
+            //             break;
+            //     }
+            // }
+
+            // public static void main(String[] args) {
+            //     // int value = 321;
+            //     // int count = 0;
+
+            //     // while (value != 0){
+            //     //     value /= 10;
+            //     //     count++;
+            //     // }
+
+            //     // do {
+            //     //     value /= 10;
+            //     //     count++;
+            //     // } while(value !=0);
+            //     // System.out.println(count);
+
+            //     for (int i = 0; i < 10; i++){
+            //         if(i % 2 != 0)
+            //             continue; // break; выполняет до первой провальной проверки условия и завершает цикл 
+            //         System.out.println(i);
+            //     }
+            // }
+            //import java.util.Scanner;
+            
+            //public class program {
+        //public static void main(String[] args) {
+
+                    // Scanner scanner = new Scanner(System.in);
+                    
+                    // System.out.print("Введите число 1: ");
+                    // int number1 = scanner.nextInt();
+                    // System.out.print("Введите число 2: ");
+                    // int number2 = scanner.nextInt();
+                    // scanner.close();
+                    // int result = number1 + number2;
+
+                    // System.out.println("Сумма чисел равно: " +result );
+
+        //}
+
+        // public static void main(String[] args) {
+        //     Scanner scanner = new Scanner(System.in);
+
+        //     System.out.print("Введите число: ");
+        //     int number = scanner.nextInt();
+        //     scanner.close();
+            
+        //     if(number != 0){
+        //         if (number % 2 == 0){
+        //             System.out.println("Число "+number+ " является четным.");
+        //         }
+           
+        //         else{
+        //             System.out.println("Число " +number+ " не является четным.");
+        //         }
+        //     } 
+
+        //     else{
+        //     System.out.println("Ваше число является нулем");
+        //     }
+        // }
+    //}
+// import java.util.Scanner;
+// public class program {
+//     public static void main(String[] args) {
+//         Scanner scanner = new Scanner(System.in);
+//         System.out.print("Введите число: ");
+//         int n = scanner.nextInt();
+//         scanner.close();
+//             int result = 0;
+//             int count = 0;
+//                 do{
+//                     result += count;
+//                     count ++;
+//                 } while(count <= n);
+//         System.out.println(result);
+//     }
+    
+// }
+
+
+
