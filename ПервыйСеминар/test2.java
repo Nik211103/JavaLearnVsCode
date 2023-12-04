f952eff4c17e483df976419d3c3febdcfd646218
Nikita <mitichkin0303@mail.ru>
2023-12-04 14:28:58 +0200

    продолжаю успешно учиться

---
+++ ПервыйСеминар/test2.java
@@ -0,0 +1,26 @@
+﻿package ПервыйСеминар;
+
+import java.util.Scanner;
+
+public class test2 {
+    public static void main(String[] args) {
+            try (Scanner scanner = new Scanner(System.in)) {
+                System.out.print("Введите свой возраст: ");
+                String age = scanner.nextLine();
+                switch (age.toLowerCase()) {//оператор .toLowerCase МОЖЕТ НЕ ОБРАЩАТЬ НА РЕГИСТРЫ 
+                    case "null":
+                        System.out.println("Ты родился");
+                        break;
+                    case "seven":
+                        System.out.println("Ты пошел в школу");
+                        break;
+                    case "18":
+                        System.out.println("Ты закончил школу");
+                        break;
+                    default:
+                        System.out.println("Неизвестный возраст");
+                        break;
+                }
+            }
+        }
+    }
---
+++ ПервыйСеминар/test3.java
@@ -0,0 +1,33 @@
+﻿package ПервыйСеминар;
+
+public class test3 {
+    // public static void main(String[] args) {
+    //     int number = 10;
+    //     int [] numbers = new int[5];
+    //     System.out.println(numbers[0]);
+    //     numbers[0] = 10; 
+    //     System.out.println(numbers[0]);
+    //     for (int i = 0; i < numbers.length; i++) {
+    //         numbers[i] = i * 10;
+    //     }
+        
+    //     for (int i = 0; i < numbers.length; i++) {
+    //         System.out.print(numbers[i]+" ");
+    //     }
+    //     System.out.println();
+    //     int [] numbers1 = {1, 2, 3};
+    //     for (int i = 0; i < numbers1.length; i++) {
+    //         System.out.print(numbers1[i]+" ");
+    //     }
+    //}
+    public static void main(String[] args) {
+        String [] str = new String[3];
+        str[0] = "Hello";
+        str[1] = "Bye";
+        str[2] = "Java";
+        
+        for (int i = 0; i < str.length; i++) {
+            System.out.println(str[i]);
+        }
+    }
+}
