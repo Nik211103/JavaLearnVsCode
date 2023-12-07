75cf455f88ee4025de2ff81d6cf1d9a20f0b06b4
Nikita <mitichkin0303@mail.ru>
2023-12-07 22:39:58 +0200

    Доделал задачки с методами

--- Практика/Test6.java
+++ Практика/Test6.java
@@ -2,90 +2,155 @@
 
 //import java.util.Scanner;
 public class Test6 {
-
-    // Создайте метод, который принимает массив строк и возвращает самую длинную строку из массива.
-    // Реализуйте метод, который принимает число и проверяет, является ли оно простым.
-    // Напишите метод, который принимает число и возвращает его факториал.
-    // Создайте метод, который принимает массив чисел и возвращает сумму всех положительных элементов.
-    // Реализуйте метод, который принимает строку и проверяет, является ли она палиндромом.
-    // Напишите метод, который принимает два числа и возвращает их произведение.
-
+    
+    
     // Создайте метод для вычисления суммы двух чисел и вызовите его из основного метода.
     //     public static void main(String[] args) {
-    //         Scanner scanner = new Scanner(System.in);
-    //         System.out.print("Введите 1 число: ");
-//         double num1 = scanner.nextDouble();
-//         System.out.print("Введите 2 число: ");
-//         double num2 = scanner.nextDouble();
-//         scanner.close();
-
-//         System.out.println("Результат вычесления: " +Calculate(num1, num2));
-//     }
-
-//     public static double Calculate(double num1, double num2){
-    //         return num1 + num2;
-    //     } 
-    
-    // Напишите метод, который принимает строку в качестве аргумента и возвращает ее длину.
-    // public static void main(String[] args) {
-        //     String string = "qwerty"; 
-        //     System.out.println("Результат: " +ArrayLenght(string));
-        // }
-    // public static int ArrayLenght(String string){
-        //     int result = string.length();
-        //     return result;
-        // }
+        //         Scanner scanner = new Scanner(System.in);
+        //         System.out.print("Введите 1 число: ");
+        //         double num1 = scanner.nextDouble();
+        //         System.out.print("Введите 2 число: ");
+        //         double num2 = scanner.nextDouble();
+        //         scanner.close();
         
-    // Реализуйте метод, который принимает массив чисел и возвращает среднее значение.
- 
+        //         System.out.println("Результат вычесления: " +Calculate(num1, num2));
+        //     }
+        
+        //     public static double Calculate(double num1, double num2){
+            //         return num1 + num2;
+            //     } 
+            
+            // Напишите метод, который принимает строку в качестве аргумента и возвращает ее длину.
+            // public static void main(String[] args) {
+                //     String string = "qwerty"; 
+                //     System.out.println("Результат: " +ArrayLenght(string));
+                // }
+                // public static int ArrayLenght(String string){
+                    //     int result = string.length();
+                    //     return result;
+                    // }
+                    
+                    // Реализуйте метод, который принимает массив чисел и возвращает среднее значение.
+                    
+                    
+                    // public static void main(String[] args) {
+                        //     Scanner scanner = new Scanner(System.in);
+                        //     System.out.print("Введите желаемое количество элементов массива: ");
+                        //     int length = scanner.nextInt();
+                        
+                        //     int[] arr = new int[length];
+                        //     ImputArray(arr);
+                        //     float result = Result(arr);
+                        //     System.out.println("Среднее арифметическое: " + result);
+                        
+                        // }
+                        
+                        // public static void ImputArray(int[] arr){
+                    //     Scanner scanner = new Scanner(System.in);
+                    //     for (int i = 0; i < arr.length; i++) {
+                        //         System.out.print("Введите числа: ");
+                        //         arr[i] = scanner.nextInt();  
+                        //     }
+                        //     scanner.close();
+                        // }
+                        
+                        // public static float Result(int[] arr){
+                            //     int sum = 0;
+                            //     for (int i = 0; i < arr.length; i++) {
+                                //         sum += arr[i];
+                                //     }
+                                //     float result = sum / arr.length;
+                                //     return result;
+                                // } 
+                                
+                                
+                                // Напишите метод, который принимает два числа и возвращает их наибольший общий делитель (НОД).
+                                // public static void main(String[] args) {
+                                    //     Scanner scanner = new Scanner(System.in);
+                                    //     System.out.print("Введите число 1: ");
+                                    //     int num1 = scanner.nextInt();
+                                    //     System.out.print("Введите число 2: ");
+                                    //     int num2 = scanner.nextInt();
+                                    //     System.out.println("Результат: "+Calculate(num1, num2));
+                                    //     scanner.close();
+                                    // }
+                                    // public static int Calculate(int num1, int num2){
+                                        //     while (num2 != 0) {
+                                            //         int temp = num2;
+                                            //         num2 = num1 % num2;
+                                            //         num1 = temp;
+                                            //     }
+                                            //     return num1;
+                                            // }
+                                            
+                                            
+                                            // Создайте метод, который принимает массив строк и возвращает самую длинную строку из массива.
+                                            
+                                            
+                                            
 
+
+
+    // Напишите метод, который принимает число и возвращает его факториал.
+    // public static void main(String[] args) {
+    //     Scanner scanner = new Scanner(System.in);
+    //     System.out.print("Введите число: ");
+    //     long num = scanner.nextLong();
+    //     scanner.close();
+    //     System.out.println("Результат равен: " +Factorial(num));
+
+    // }
+
+    // public static long Factorial(long num){
+    //     if (num == 0){
+    //         return 1;
+    //     }
+    //     else {
+    //         return num * Factorial(num - 1);
+    //     }
+    // }
+
+
+// Создайте метод, который принимает массив чисел и возвращает сумму всех положительных элементов.
+
+    
     // public static void main(String[] args) {
     //     Scanner scanner = new Scanner(System.in);
     //     System.out.print("Введите желаемое количество элементов массива: ");
     //     int length = scanner.nextInt();
-    
+                        
     //     int[] arr = new int[length];
     //     ImputArray(arr);
-    //     float result = Result(arr);
-    //     System.out.println("Среднее арифметическое: " + result);
-    
-    // }
-    
-    // public static void ImputArray(int[] arr){
-        //     Scanner scanner = new Scanner(System.in);
-        //     for (int i = 0; i < arr.length; i++) {
-            //         System.out.print("Введите числа: ");
-            //         arr[i] = scanner.nextInt();  
-            //     }
-            //     scanner.close();
-            // }
-            
-            // public static float Result(int[] arr){
-                //     int sum = 0;
-                //     for (int i = 0; i < arr.length; i++) {
-                    //         sum += arr[i];
-                    //     }
-                    //     float result = sum / arr.length;
-                    //     return result;
+        
+    //     System.out.println("Результат равен: " +Result(arr) );
+    //     scanner.close();
+                        
     // } 
 
 
-    // Напишите метод, который принимает два числа и возвращает их наибольший общий делитель (НОД).
-    // public static void main(String[] args) {
+    // public static void ImputArray(int[] arr){
     //     Scanner scanner = new Scanner(System.in);
-    //     System.out.print("Введите число 1: ");
-    //     int num1 = scanner.nextInt();
-    //     System.out.print("Введите число 2: ");
-    //     int num2 = scanner.nextInt();
-    //     System.out.println("Результат: "+Calculate(num1, num2));
-        
-    // }
-    // public static int Calculate(int num1, int num2){
-    //     while (num2 != 0) {
-    //         int temp = num2;
-    //         num2 = num1 % num2;
-    //         num1 = temp;
+    //     for (int i = 0; i < arr.length; i++) {
+    //         System.out.print("Введите числа: ");
+    //         arr[i] = scanner.nextInt();  
     //     }
-    //     return num1;
+    //     scanner.close();
     // }
-}
+
+
+    // public static int Result(int [] arr){
+    //     int result = 0;
+    //     for (int i = 0; i < arr.length; i++) {
+    //         if(arr[i] > 0){
+    //             result += arr[i];
+    //         }
+    //     }
+    //     return result;
+    // }
+
+
+
+
+
+}                                                         
+        
