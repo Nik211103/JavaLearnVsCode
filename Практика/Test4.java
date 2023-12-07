288412b41090ceece552ac638b448f56b7e01ff0
Nikita <mitichkin0303@mail.ru>
2023-12-07 12:38:45 +0200

    смотрю видосы на ютубе и пытаюсь понять методы set и get

--- Практика/Test4.java
+++ Практика/Test4.java
@@ -3,33 +3,41 @@
 public class Test4 {
     public static void main(String[] args) {
         Person person1 = new Person();
-        // person1.name = "Роман";
-        person1.age = 50;
+        person1.setNameAndAge("Роман", 20);
+        
+        String s1 = "Вова";
         // person1.sayHello();
         int year1 = person1.calculate();
+        person1.speak();
         
         //System.out.println("Меня зовут " + person1.name +" "+"мне "+person1.age+" лет." );
         
         Person person2 = new Person();
         person2.calculate();
-        // person2.name = "Вова";
+        person2.name = "Вова";
         int year2 =  person2.age = 20;
         //System.out.println("Меня зовут " + person2.name +" "+"мне "+person2.age+" лет." );
         // person2.speak();
-        System.out.println("Первому человеку до пенсии: " +year1);
-        System.out.println("Второму человеку до пенсии: " +year2);
+        person2.setNameAndAge(s1, 30);
+        person1.speak();
+        person2.speak();
         
     }
 }
 
 
-class Person { // У класса могуть быть: 1. Данные(поля); 2. Действия, которые он модет совершать (методы)
+class Person { // У класса могуть быть: 1. Данные(поля); 2. Действия, которые он может совершать (методы)
     String name;
     int age;
 
     int calculate(){
         int years = 65 - age;
         return years;
+    }
+
+    void setNameAndAge(String username, int userage){
+        name = username;
+        age =userage;
     }
 
     void speak(){
---
+++ Практика/Test5.java
@@ -0,0 +1,44 @@
+﻿package Практика;
+
+public class Test5 {
+    public static void main(String[] args) {
+        
+        Person person1 = new Person();
+        person1.setName("Какое-то имя");
+        person1.setAge(12);
+        System.out.println("Выводим значение " +person1.getName());
+        System.out.println("Выводим значение " +person1.getAge());
+        
+    }
+}
+
+
+class Person { // У класса могуть быть: 1. Данные(поля); 2. Действия, которые он может совершать (методы)
+    private String name;
+    private int age;
+
+
+    public void setName(String userName){
+        name = userName;
+    }
+    public String getName(){
+       return name;
+    }
+
+    public void setAge(int userAge){
+        age = userAge;
+    }
+    public int getAge(){
+       return age;
+    }
+    
+
+    void speak(){
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Меня зовут " + name + ", мне " + age + " лет.");
+        }
+    }
+    void sayHello(){
+        System.out.println("Hello");
+    }
+}
