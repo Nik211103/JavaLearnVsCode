7c2285a1bf07ea92fb7e4968f3cc2f84c3289871
Nikita <mitichkin0303@mail.ru>
2023-12-11 23:25:29 +0200

    Учусь по ютубу, все норм

--- Практика/Test10.java
+++ Практика/Test10.java
@@ -2,19 +2,37 @@
 
 public class Test10 {
     public static void main(String[] args) {
-        Human human1 = new Human();
+        Human h1 = new Human("bob", 30);//Варьируя методы Human можно задавать разные параметры на вход
+        Human h2 = new Human("Tom", 12);
+        //h1.setName("Vova");
+        //Human.description = "Nice";//Статические переменные можно использовать и вызывать через класс,
+        // она является общей для всех
+        // Human.getDescription();
+        //h1.getAll();
+        //human1.description = "Bad";// так вообще нельзя, но в принципе можно, но не стоит
+        //h2.getAll();
+        Human h3 = new Human("rob", 4);
         
+        h1.printNumPeople();
+        h2.printNumPeople();
+        h3.printNumPeople();
+
+        //Math.pow(2, 4);//2 в 4
+
     }
 }
 class Human{
     private String name;
     private int age;
 
-    public Human(){
-        this.name = "Имя по умолчянию";
-        this.age = 0;
-        
-    }
+    private static int countPeople;
+    public static String description;
+
+    // public Human(){
+    //     this.name = "Имя по умолчянию";
+    //     this.age = 0;
+                
+    // }
 
     // public Human(String name){//перегрузка метода для 2х методов
     //     System.out.println("Привет из 2 конструтора");
@@ -24,7 +42,8 @@ class Human{
     public Human(String name, int age){
         this.name = name;
         this.age = age;
-        System.out.println("Привет из 3 конструктора");
+        //System.out.println("Привет из 3 конструктора");
+        countPeople++;
     }
 
 
@@ -45,4 +64,23 @@ class Human{
     public void getInfo(){
         System.out.println(name + " "+ age);
     }
+
+    // public static void getDescription(){
+    //     System.out.println(description);
+    // }
+
+    public void getAll(){
+        System.out.println(name+" "+ age +" "+description);
+    }
+
+    // public static void printAll(){
+    //     System.out.println(name);//нельзя обращаться к name т.к.
+    // она является переменной объекта у которого переменная name каждая индивидуальной,
+    //     // а name статически
+    // }
+        public void printNumPeople(){
+            System.out.println(countPeople);
+        }
+
 }
+
---
+++ Практика/Test11.java
@@ -0,0 +1,20 @@
+﻿package Практика;
+
+public class Test11 {
+    public static void main(String[] args) {
+        new Test(20);
+        final int X = 10;//невозможно изменить ее значение
+        System.out.println(X);
+    }
+}
+class Test{
+    public final int CONST; //public static final int CONST; делается для делимости между всеми, иначе переполнение
+
+    public Test(int CONST){
+        this.CONST = CONST;
+    }
+
+    // public void setCONST(int x){ так не получится т.к.
+    //     this.CONST = x; переменная константа
+    // }
+}
